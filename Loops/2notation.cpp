e99cdcf876b55ade8d359822e16f1ef35fcdc2ac
K0TblK <90197664+K0TblK@users.noreply.github.com>
2021-12-17 11:45:51 +0300

    Create 2notation.cpp

---
+++ Loops/2notation.cpp
@@ -0,0 +1,17 @@
+//--------Nineth task
+	int num,deg=1;
+	std::cout << "Input integer: ";
+	std::cin >> num;
+	int num_tmp = num;
+	while (num_tmp != 1) {
+		num_tmp/=2;
+		deg *= 2;
+		std::cout << deg << "\n";
+	}
+	std::cout << "\n" << "Your number is: ";
+	while (deg != 0) {
+		std::cout << (num / deg);
+		num %= deg;
+		deg /= 2;
+	}
+	return(0);
