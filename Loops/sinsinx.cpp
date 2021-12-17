0615aa3b9440ab0d7ba7cf37da688fd1aecab786
K0TblK <90197664+K0TblK@users.noreply.github.com>
2021-12-17 11:36:46 +0300

    Finds sin of sin of sin of x

---
+++ Loops/sinsinx.cpp
@@ -0,0 +1,13 @@
+//--------Third task
+	int n;
+	double x, sum=0;
+	std::cout << "Input amount of elements: ";
+	std::cin >> n;
+	std::cout << "\n" << "Input argument: ";
+	std::cin >> x;
+	for (int i = 0; i < n; ++i) {
+		sum += sin(x);
+		x = sin(x);
+	}
+	std::cout << "Summary of subsequence: " << sum << "\n";
+	return(0);
