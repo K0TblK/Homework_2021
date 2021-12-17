cb39e7bd235ca9b119070acee3ae8593e51df4ae
K0TblK <90197664+K0TblK@users.noreply.github.com>
2021-12-17 11:35:01 +0300

    Summary of 1/n subsequence

---
+++ Loops/1toN.cpp
@@ -0,0 +1,10 @@
+//--------First task
+	int n;
+	double sum = 1;
+	std::cout << "input n: ";
+	std::cin >> n;
+	for (int i = 2; i <= n; ++i) {
+		sum += 1.0 / i;
+	}
+	std::cout << "summary of subsequence: " << sum << "\n";
+	return(0);
