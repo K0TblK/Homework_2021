5c4cf79d7eb6e2767ca73640266a3d859ae50d13
K0TblK <90197664+K0TblK@users.noreply.github.com>
2021-12-17 12:02:05 +0300

    4 task

--- Loops/pi.cpp
+++ Loops/pi.cpp
@@ -1,4 +1,5 @@
-//--------Forth task
+#include<iostream>
+int main(){
 	double eps, sum = 4, sign = -1, counter = 1;
 	std::cout << "Input epsilon: ";
 	std::cin >> eps;
@@ -9,3 +10,4 @@
 	} while (4 * abs(1.0 / (2 * counter + 1)) >= eps);
 	std::cout << "Summary of subsequence: " << sum << "\n";
 	return (0);
+}
