056cd6dda6a7008975f5e401556d6e0315e430ed
K0TblK <90197664+K0TblK@users.noreply.github.com>
2021-12-17 11:37:31 +0300

    Finds value of pi

---
+++ Loops/pi.cpp
@@ -0,0 +1,11 @@
+//--------Forth task
+	double eps, sum = 4, sign = -1, counter = 1;
+	std::cout << "Input epsilon: ";
+	std::cin >> eps;
+	do {
+		sum += 4 * sign * (1.0/(2*counter+1));
+		sign *= -1;
+		++counter;
+	} while (4 * abs(1.0 / (2 * counter + 1)) >= eps);
+	std::cout << "Summary of subsequence: " << sum << "\n";
+	return (0);
