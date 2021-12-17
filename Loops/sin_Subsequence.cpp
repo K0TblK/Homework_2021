849f706abaa6bee77c6069acdbed200e20130600
K0TblK <90197664+K0TblK@users.noreply.github.com>
2021-12-17 11:41:56 +0300

    Create sin_Subsequence.cpp

---
+++ Loops/sin_Subsequence.cpp
@@ -0,0 +1,18 @@
+//--------Fifth task
+	double xmin, xmax, dx, eps;
+	std::cout << "Input xmin, xmax, dx and epsilon: ";
+	std::cin >> xmin >> xmax >> dx >> eps;
+	for (xmin; xmin <= xmax; xmin += dx) {
+		double power = xmin;
+		int counter = 1;
+		double curr = xmin / counter;
+		double fx=0;
+			while (abs(curr) >= eps) {
+				counter += 2;
+				fx += curr;
+				curr= -1*curr*(xmin*xmin/(counter*(counter-1)));
+				power *= power * power;
+		}
+		std::cout << "x = " << xmin << "; sin(x) = " << sin(xmin) << "; fx=: " << fx  << "\n";
+	}
+	return (0);
