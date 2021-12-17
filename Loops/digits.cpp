c78ace78d87438dd1900bc66de6fa3e2bb4c55a5
K0TblK <90197664+K0TblK@users.noreply.github.com>
2021-12-17 11:44:38 +0300

    Finds summary and multiplication of digits

---
+++ Loops/digits.cpp
@@ -0,0 +1,12 @@
+//----------Eighth task
+	int num, sum=0, prod=1;
+	std::cout << "Input integer: ";
+	std::cin >> num;
+	while (num!=0){
+		int digit = num % 10;
+		sum += digit;
+		prod *= digit;
+		num /= 10;
+	}
+	std::cout << "\n" << "Summary of digits: " << sum << "; Product of digits: " << prod << "\n";
+	return(0);
