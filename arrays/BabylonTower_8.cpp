22cd120fc858bead34a091450451c335b2682201
K0TblK <90197664+K0TblK@users.noreply.github.com>
2021-12-17 12:07:02 +0300

    8 task

--- arrays/BabylonTower_8.cpp
+++ arrays/BabylonTower_8.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <math.h>
 int main() {
-    int N;
+    int n;
     std::cout << "Input room's number: ";
-    std::cin >> N;
+    std::cin >> n;
     int nk = 0;
     int floor = 1;
-    for (int i = 1; i <= 1000; ++i) {
+    for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= i; ++j) {
             for (int k = 1; k <= i;++k) {
                 ++nk; 
-                if (N == nk) {
+                if (n == nk) {
                     std::cout << "\n The room is on the " << floor << "th floor and in " << k << "th flat";
                     break;
                 }
