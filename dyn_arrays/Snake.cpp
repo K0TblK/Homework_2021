92ca3d14bae3a5526e6fda725986fa0aad537219
K0TblK <90197664+K0TblK@users.noreply.github.com>
2021-12-18 03:06:05 +0300

    Added parameter for direction

--- dyn_arrays/Snake.cpp
+++ dyn_arrays/Snake.cpp
@@ -1,23 +1,42 @@
 #include<iostream>
-int main(){
-    int n,m, counter=1;
+int main() {
+    int n, m, dir, counter = 1;
     std::cout << "Input amount of rows: ";
     std::cin >> n;
     std::cout << "\n" << "Input amount of columns: ";
     std::cin >> m;
-    int** Matrix = new int*[n];
+    std::cout << "\n" << "Input direction:" << "\n";
+    std::cout << "1 - from left to right; 2 - from right to left" << "\n";
+    std::cin >> dir;
+    int** Matrix = new int* [n];
     for (int i = 0; i < n; ++i) {
         Matrix[i] = new int[m];
     }
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            if (i % 2 == 0) {
-                Matrix[i][j]=counter;
-                ++counter;
+    if (dir == 1) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (i % 2 == 0) {
+                    Matrix[i][j] = counter;
+                    ++counter;
+                }
+                else {
+                    Matrix[i][m - j - 1] = counter;
+                    ++counter;
+                }
             }
-            else {
-                Matrix[i][m-j-1] = counter;
-                ++counter;
+        }
+    }
+    else if (dir == 2) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = m-1; j >= 0; --j) {
+                if (i % 2 == 0) {
+                    Matrix[i][j] = counter;
+                    ++counter;
+                }
+                else {
+                    Matrix[i][m - j - 1] = counter;
+                    ++counter;
+                }
             }
         }
     }
