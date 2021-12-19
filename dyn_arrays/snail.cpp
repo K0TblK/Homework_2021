7bd0aed8dadc0ba5e27647b6ebc01414e8405345
K0TblK <90197664+K0TblK@users.noreply.github.com>
2021-12-19 16:39:41 +0300

    Added direction

--- dyn_arrays/snail.cpp
+++ dyn_arrays/snail.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
-int main(){
-    int n, m;
+int main() {
+    int n, m, dir;
     std::cout << "Input amount of rows: ";
     std::cin >> n;
     std::cout << "Input amount of columns: ";
     std::cin >> m;
+    std::cout << "\n" << "Input direction:" << "\n";
+    do {
+        std::cout << "1 - from left to right; 2 - from top to bottom" << "\n";
+        std::cin >> dir;
+    } while ((dir != 1) && (dir != 2));
     int top = 0;
     int bot = n;
     int left = 0;
@@ -14,27 +19,53 @@ int main(){
     for (int i = 0; i < n; ++i) {
         Matrix[i] = new int[n];
     }
-    while (counter != (n * m) + 1) {
-        for (int i = left; i < right; ++i) {
-            Matrix[top][i] = counter;
-            ++counter;
+    if (dir == 1) {
+        while (counter != (n * m) + 1) {
+            for (int i = left; i < right; ++i) {
+                Matrix[top][i] = counter;
+                ++counter;
+            }
+            ++top;
+            for (int i = top; i < bot; ++i) {
+                Matrix[i][right - 1] = counter;
+                ++counter;
+            }
+            --right;
+            for (int i = right - 1; i > left; --i) {
+                Matrix[bot - 1][i] = counter;
+                ++counter;
+            }
+            --bot;
+            for (int i = bot; i >= top; --i) {
+                Matrix[i][left] = counter;
+                ++counter;
+            }
+            ++left;
         }
-        ++top;
-        for (int i = top; i < bot; ++i) {
-            Matrix[i][right - 1] = counter;
-            ++counter;
+    }
+    if (dir == 2) {
+        while (counter != (n * m) + 1) {
+            for (int i = top; i < bot; ++i) {
+                Matrix[i][left] = counter;
+                ++counter;
+            }
+            ++left;
+            for (int i = left; i < right; ++i) {
+                Matrix[bot - 1][i] = counter;
+                ++counter;
+            }
+            --bot;
+            for (int i = bot - 1; i >= top; --i) {
+                Matrix[i][right - 1] = counter;
+                ++counter;
+            }
+            --right;
+            for (int i = right - 1; i >= left; --i) {
+                Matrix[top][i] = counter;
+                ++counter;
+            }
+            ++top;
         }
-        --right;
-        for (int i = right - 1; i > left; --i) {
-            Matrix[bot - 1][i] = counter;
-            ++counter;
-        }
-        --bot;
-        for (int i = bot; i >= top; --i) {
-            Matrix[i][left] = counter;
-            ++counter;
-        }
-        ++left;
     }
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
@@ -42,5 +73,9 @@ int main(){
         }
         std::cout << "\n";
     }
+    for (size_t i = 0; i < n; ++i) {
+        delete[] Matrix[m];
+    }
+    delete[] Matrix;
     return 0;
 }
